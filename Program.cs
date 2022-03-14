6fd43cb8e4496d9e87d51cf4a99c510dbcac606d
democraz20 <mozartrasig@gmail.com>
2022-03-14 23:03:03 +0700

    borders now work on all four sides
    awaiting adding a text in the middle or the corner
    
    needed 2 loops for the sides because alignments are different
    
    drawing borders now is a function to be called so main looks cleaner
    
    added optimization, only draws everytime the resolution changes

--- Program.cs
+++ Program.cs
@@ -10,33 +10,62 @@ namespace consoleresize
         static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.White;
-            //int width = Console.WindowWidth;
-            //int height = Console.WindowHeight;
+            int width = Console.WindowWidth;
+            int height = Console.WindowHeight;
             //Console.WriteLine("Current window width = "+width);
             //Console.WriteLine("Current window height = "+height);
+            Console.WriteLine(Console.LargestWindowHeight);
+            Console.WriteLine(Console.LargestWindowWidth);
             for(;;){
                 Thread.Sleep(1000);
                 //int width = Console.WindowWidth;
-                int height = Console.WindowHeight;
                 //Console.WriteLine("W : "+Console.WindowWidth+" H : "+Console.WindowHeight);
-                Console.BackgroundColor = ConsoleColor.Blue;
-                Console.Clear();
-                Console.BackgroundColor=ConsoleColor.Gray;
-                for (int i = 0; i < Console.WindowWidth; i++)
-                {
+                if(width != Console.WindowWidth || height != Console.WindowHeight){
+                    //reset values
+                    width = Console.WindowWidth;
+                    height = Console.WindowHeight;
+                    //prepare console curser 
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.Clear();
                     Console.BackgroundColor=ConsoleColor.Gray;
-                    utils.WriteAt(" ", i, height-1);
-                    Console.BackgroundColor=ConsoleColor.DarkGray;
-                    utils.WriteAt(" ", i, height);
-                    Console.BackgroundColor=ConsoleColor.Gray;
-                    utils.WriteAt(" ", i, 0);
-                    Console.BackgroundColor=ConsoleColor.DarkGray;
-                    utils.WriteAt(" ", i, 1);
-                    
+                    //draw
+                    draw.borders(height, width);
                 }
-                
-                
+            }
+        }
+    }
 
+    class draw{
+        public static void borders(int height, int width){
+            //top bottom
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                //bottom layer  
+                Console.BackgroundColor=ConsoleColor.Gray;
+                utils.WriteAt(" ", i, height-2);
+                Console.BackgroundColor=ConsoleColor.DarkGray;
+                utils.WriteAt(" ", i, height-1);
+                //top layer
+                Console.BackgroundColor=ConsoleColor.Gray;
+                utils.WriteAt(" ", i, 0);
+                Console.BackgroundColor=ConsoleColor.DarkGray;
+                utils.WriteAt(" ", i, 1);
+                
+            }
+            for (int j = 0; j < Console.WindowHeight-3; j++)
+            {
+                //left side
+                Console.BackgroundColor=ConsoleColor.Gray;
+                utils.WriteAt("  ", 0,j+1);
+                Console.BackgroundColor=ConsoleColor.DarkGray;
+                utils.WriteAt("  ", 2,j+1);
+            }
+            for (int i = 0; i < Console.WindowHeight-1; i++)
+            {                
+                Console.BackgroundColor=ConsoleColor.Gray;
+                utils.WriteAt("  ", width-4,i);
+                Console.BackgroundColor=ConsoleColor.DarkGray;
+                utils.WriteAt("  ", width-2,i);
             }
         }
     }
